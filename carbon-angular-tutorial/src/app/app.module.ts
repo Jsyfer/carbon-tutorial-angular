769b7840d6af663cbc7b158ae716815349925cc5
jsyfer <goldensundd@hotmail.com>
2022-10-13 11:19:15 +0800

    move icon to header

--- carbon-angular-tutorial/src/app/app.module.ts
+++ carbon-angular-tutorial/src/app/app.module.ts
@@ -6,10 +6,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 // carbon-components-angular default imports
 import { UIShellModule, IconModule } from 'carbon-components-angular';
-import Notification20 from '@carbon/icons/es/notification/20';
-import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
-import AppSwitcher20 from '@carbon/icons/es/app-switcher/20';
-import { IconService } from "carbon-components-angular";
 
 @NgModule({
   declarations: [
@@ -25,10 +21,5 @@ import { IconService } from "carbon-components-angular";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-  constructor(protected iconService: IconService) { }
-
-  ngOnInit(): void {
-    this.iconService.registerAll([Notification20]);
-  }
+export class AppModule{
 }
--- carbon-angular-tutorial/src/app/header/header.component.ts
+++ carbon-angular-tutorial/src/app/header/header.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit,HostBinding  } from '@angular/core';
+import { Component, OnInit, HostBinding  } from '@angular/core';
+import Notification20 from '@carbon/icons/es/notification/20';
+import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons/es/app-switcher/20';
+import { IconService } from 'carbon-components-angular';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -6,7 +10,8 @@ import { Component, OnInit,HostBinding  } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @HostBinding('class.cds--header') headerClass = true;
-  constructor() { }
+  constructor(protected iconService: IconService) { }
   ngOnInit(): void {
+    this.iconService.registerAll([Notification20]);
   }
 }
